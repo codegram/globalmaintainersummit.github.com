35ef26695497a6582b5bb4e021594603ab343ae1
Verónica Ojeda <glebapps@gmail.com>
2021-04-08 13:53:36 +0200

    fetch maintainers and cta in each component and move main to default layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,16 @@
 <template>
-  <main class="main">
-    <section>
-      <Hero />
-      <FloatingCards :content="maintainers" />
-      <Lead />
-      <Details />
-      <div class="cta">
-        <CommonLink button :href="cta.url">
-          {{ cta.label }}
-        </CommonLink>
-      </div>
-      <Maintainers :content="maintainers" />
-    </section>
-  </main>
+  <section>
+    <Hero />
+    <FloatingCards />
+    <Lead />
+    <Details />
+    <Cta />
+    <Maintainers />
+  </section>
 </template>
-<script>
-export default {
-  async asyncData({ $content }) {
-    const { cta, maintainers } = await $content('home').fetch()
-
-    return {
-      cta,
-      maintainers,
-    }
-  },
-}
-</script>
 
 <style lang="scss" scoped>
-.main {
-  position: relative;
-  width: 100%;
-  overflow: hidden;
-}
 section {
   @include section();
-}
-.cta {
-  margin-top: 88px;
 }
 </style>
