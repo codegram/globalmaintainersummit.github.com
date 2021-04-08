cf3cc1b76b793bee6aaa4cff32e57cd1d1c580c0
Verónica Ojeda <glebapps@gmail.com>
2021-04-08 12:11:25 +0200

    apply PR suggestions

--- components/Lead.vue
+++ components/Lead.vue
@@ -3,7 +3,7 @@
     <div class="lead">
       <h2 class="lead__title">{{ content.title }}</h2>
       <CommonLink :href="calendarUrl">
-        <span class="lead__dates">{{ content.dates }}</span>
+        <span class="lead__dates">{{ dates }}</span>
         <AddToCalIcon />
       </CommonLink>
     </div>
@@ -25,9 +25,46 @@ export default {
   },
   data() {
     return {
-      calendarUrl:
-        'http://www.google.com/calendar/event?action=TEMPLATE&dates=20210608%2F20210610&text=Global%20Maintainer%20Event&&details=A%20virtual%20gathering%20for%20the%20maintainers%20that%20make%20open%20source%20possible',
+      locale: 'en-US',
+      startDate: new Date(this.content.dates.start),
+      endDate: new Date(this.content.dates.end),
     }
+  },
+  computed: {
+    dates() {
+      const startDay = this.startDate.getDate()
+      const startMonth = this.startDate.toLocaleDateString(this.locale, {
+        month: 'long',
+      })
+      const endDay = this.endDate.getDate()
+      const endMonth = this.endDate.toLocaleDateString(this.locale, {
+        month: 'long',
+      })
+      const year = this.startDate.getFullYear()
+
+      return startMonth === endMonth
+        ? `${startMonth} ${startDay}-${endDay}, ${year}`
+        : `${startMonth} ${startDay} - ${endMonth}${endDay}, ${year}`
+    },
+    calendarUrl() {
+      const startDate = this.formatDateCalendar(this.startDate)
+      // In order to set an all day event in Google Calendar, the end day has to be a day more of the actual date
+      // e.g. To create an event for dates June 8-9 it needs to be set June 8-10
+      const endDate = this.formatDateCalendar(this.nextDay(this.endDate))
+      const text = 'Global Maintainer Event'
+      const details =
+        'A virtual gathering for the maintainers that make open source possible'
+
+      return `http://www.google.com/calendar/event?action=TEMPLATE&dates=${startDate}%2F${endDate}&text=${text}&&details=${details}`
+    },
+  },
+  methods: {
+    formatDateCalendar(date) {
+      return date.toISOString().slice(0, 10).replaceAll('-', '') // YYYYMMDD
+    },
+    nextDay(date) {
+      return new Date(date.setDate(date.getDate() + 1))
+    },
   },
 }
 </script>
