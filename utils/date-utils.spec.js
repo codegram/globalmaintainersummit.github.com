dd8dff018b39c14264bcf7889102851a0b16f279
Verónica Ojeda <glebapps@gmail.com>
2021-04-13 12:20:24 +0200

    remove timezone test
    Co-authored-by: David Morcillo <david.morcillo@gmail.com>

--- utils/date-utils.spec.js
+++ utils/date-utils.spec.js
@@ -3,6 +3,7 @@ import { formatDate, buildGoogleCalendarUrl } from '~/utils/date-utils.js'
 describe('Date utils', () => {
   const startDate = new Date('2021-06-08')
   const endDate = new Date('2021-06-09')
+
   describe('Format Date', () => {
     it('formats dates that belong to the same month', () => {
       const actualDate = formatDate(startDate, endDate)
@@ -15,17 +16,6 @@ describe('Date utils', () => {
     it('formats dates that belong to the different months', () => {
       const actualDate = formatDate(startDate, new Date('2021-07-09'))
       const expectedDate = 'June 8 - July 9, 2021'
-
-      expect(actualDate).not.toBeUndefined()
-      expect(actualDate).toEqual(expectedDate)
-    })
-
-    it('formats dates correctly without taking into account that they are in a different timezone', () => {
-      const actualDate = formatDate(
-        new Date('2021-06-08T00:01:00.000+08:00'),
-        new Date('2021-06-09T00:01:00.000+08:00')
-      )
-      const expectedDate = 'June 8-9, 2021'
 
       expect(actualDate).not.toBeUndefined()
       expect(actualDate).toEqual(expectedDate)
